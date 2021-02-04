68060fd6ef626061fcf2d4b91d3ba7b7d6472c36
anatawa12 <anatawa12@icloud.com>
2021-02-04 17:32:10 +0900

    make all AutoVisitorAnnotationErrors readonly

--- compiler/src/main/java/com/anatawa12/autoVisitor/compiler/common/AutoVisitorAnnotationErrors.java
+++ compiler/src/main/java/com/anatawa12/autoVisitor/compiler/common/AutoVisitorAnnotationErrors.java
@@ -18,37 +18,37 @@ public class AutoVisitorAnnotationErrors {
 
     // @GenerateVisitor
     @NotNull
-    public static DiagnosticFactory0<PsiElement> GENERATE_VISITOR_FOR_NON_ABSTRACT = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> GENERATE_VISITOR_FOR_NON_ABSTRACT = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> VISITOR_MUST_HAVE_NO_ARG_CONSTRUCTOR = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> VISITOR_MUST_HAVE_NO_ARG_CONSTRUCTOR = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> VISITOR_CANNOT_HAVE_ABSTRACTS = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> VISITOR_CANNOT_HAVE_ABSTRACTS = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> VISITOR_OF_NON_CLASS = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> VISITOR_OF_NON_CLASS = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> TARGET_CLASS_DOESNT_HAVE_VISITOR = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> TARGET_CLASS_DOESNT_HAVE_VISITOR = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory2<PsiElement, KotlinType, KotlinType> VISITOR_OF_TARGET_IS_NOT_THIS_CLASS = DiagnosticFactory2.create(Severity.ERROR);
+    public static final DiagnosticFactory2<PsiElement, KotlinType, KotlinType> VISITOR_OF_TARGET_IS_NOT_THIS_CLASS = DiagnosticFactory2.create(Severity.ERROR);
 
     // @HasVisitor
     @NotNull
-    public static DiagnosticFactory0<PsiElement> VISITOR_TYPE_IS_NOT_ABSTRACT_CLASS = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> VISITOR_TYPE_IS_NOT_ABSTRACT_CLASS = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory1<PsiElement, KClassValue.Value> INVALID_SUBCLASS = DiagnosticFactory1.create(Severity.ERROR);
+    public static final DiagnosticFactory1<PsiElement, KClassValue.Value> INVALID_SUBCLASS = DiagnosticFactory1.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory2<PsiElement, KotlinType, KotlinType> NO_HAS_ACCEPT_AT = DiagnosticFactory2.create(Severity.ERROR);
+    public static final DiagnosticFactory2<PsiElement, KotlinType, KotlinType> NO_HAS_ACCEPT_AT = DiagnosticFactory2.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> ACCEPT_FUNCTION_NOT_FOUND = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> ACCEPT_FUNCTION_NOT_FOUND = DiagnosticFactory0.create(Severity.ERROR);
 
     // common: Visitor class
     @NotNull
-    public static DiagnosticFactory1<PsiElement, Integer> VISITOR_HAS_INVALID_COUNT_OF_TYPE_PARAM = DiagnosticFactory1.create(Severity.ERROR);
+    public static final DiagnosticFactory1<PsiElement, Integer> VISITOR_HAS_INVALID_COUNT_OF_TYPE_PARAM = DiagnosticFactory1.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> VISITOR_RETURN_TYPE_IS_IN_TYPE_VARIABLE = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> VISITOR_RETURN_TYPE_IS_IN_TYPE_VARIABLE = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> VISITOR_DATA_PARAM_IS_OUT_TYPE_VARIABLE = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> VISITOR_DATA_PARAM_IS_OUT_TYPE_VARIABLE = DiagnosticFactory0.create(Severity.ERROR);
     @NotNull
-    public static DiagnosticFactory0<PsiElement> VISITOR_TYPE_VARIABLE_MUST_NOT_HAVE_BOUNDS = DiagnosticFactory0.create(Severity.ERROR);
+    public static final DiagnosticFactory0<PsiElement> VISITOR_TYPE_VARIABLE_MUST_NOT_HAVE_BOUNDS = DiagnosticFactory0.create(Severity.ERROR);
 
     static {
 
