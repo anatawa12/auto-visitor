5c056b08bc9caa5a73fc9b5725427c322dac09af
anatawa12 <anatawa12@icloud.com>
2021-03-09 19:34:27 +0900

    make annotation-value-gen as compileOnly

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(project(":lib"))
-    implementation(project(":annotation-value-gen"))
+    compileOnly(project(":annotation-value-gen"))
     kapt(project(":annotation-value-gen"))
     compileOnly("com.google.auto.service:auto-service-annotations:1.0-rc7")
     kapt("com.google.auto.service:auto-service:1.0-rc7")
