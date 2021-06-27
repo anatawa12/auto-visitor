8600f27f4aee824c146b395f9b1b4e5839ea5060
anatawa12 <anatawa12@icloud.com>
2021-06-27 16:06:43 +0900

    remove configurations only for maven central

--- gradle-scripts/publish-to-central-java.gradle.kts
+++ gradle-scripts/publish-to-central-java.gradle.kts
@@ -5,13 +5,6 @@ val org.gradle.api.Project.`publishing`: org.gradle.api.publish.PublishingExtens
 fun org.gradle.api.Project.`publishing`(configure: org.gradle.api.publish.PublishingExtension.() -> Unit): Unit =
     (this as org.gradle.api.plugins.ExtensionAware).extensions.configure("publishing", configure)
 
-val org.gradle.api.Project.`signing`: org.gradle.plugins.signing.SigningExtension
-    get() =
-        (this as org.gradle.api.plugins.ExtensionAware).extensions.getByName("signing") as org.gradle.plugins.signing.SigningExtension
-
-fun org.gradle.api.Project.`signing`(configure: org.gradle.plugins.signing.SigningExtension.() -> Unit): Unit =
-    (this as org.gradle.api.plugins.ExtensionAware).extensions.configure("signing", configure)
-
 val org.gradle.api.Project.`java`: org.gradle.api.plugins.JavaPluginExtension
     get() =
         (this as org.gradle.api.plugins.ExtensionAware).extensions.getByName("java") as org.gradle.api.plugins.JavaPluginExtension
@@ -20,7 +13,6 @@ fun org.gradle.api.Project.`java`(configure: org.gradle.api.plugins.JavaPluginEx
     (this as org.gradle.api.plugins.ExtensionAware).extensions.configure("java", configure)
 
 apply(plugin = "maven-publish")
-apply(plugin = "signing")
 
 publishing.publications.create<MavenPublication>("maven") {
     from(components["java"])
@@ -31,5 +23,3 @@ java {
     withSourcesJar()
     withJavadocJar()
 }
-
-apply(from = "${rootProject.projectDir}/gradle-scripts/publish-to-central.gradle.kts")
